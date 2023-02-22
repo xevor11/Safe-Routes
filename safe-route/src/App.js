58d2774d4a91475797cffc50505b995c30ce8b4f
ngentekis <ngentekis@gmail.com>
2023-02-21 20:00:33 -0600

    top bar WIP for app.js

--- safe-route/src/App.js
+++ safe-route/src/App.js
@@ -2,21 +2,38 @@ import React from 'react';
 import './App.css';
 import LoginButton from './components/login-button';
 import LogoutButton from './components/logout-button';
-import MapCont from './components/mapcontainer';
 import SignupButton from './components/signup-button';
+import MapCont from './components/mapcontainer';
+import SearchBar from './components/search-bar';
+import TopBar from './components/topbar';
+
 
 function App() {
 
-
   return (
-    
-    <>
-      <LoginButton/>
-      <LogoutButton/>
-      <SignupButton/>
+    <div>
+      <TopBar />
       <MapCont/>
-    </>
-  );  
+    </div>
+
+  );
 }
 
 export default App;
+
+
+{/* <div
+      style={{
+        display:"flex",
+        flexDirection:"column",
+        width:"auto",
+        height:"100%",
+      }}>
+      <nav style={{width:"20%", height:"10%", justifyContent:"space-between", display:"flex"}}>
+        <SearchBar/>
+        <LoginButton/>
+      </nav>
+      <div>
+        <MapCont/>
+      </div>
+    </div> */}
