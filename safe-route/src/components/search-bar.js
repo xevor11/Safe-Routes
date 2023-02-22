8654ee96cf61c7c4fee524c4c0de7d58eb391424
ngentekis <ngentekis@gmail.com>
2023-02-21 19:59:33 -0600

    added search bar component

---
+++ safe-route/src/components/search-bar.js
@@ -0,0 +1,14 @@
+import React from "react";
+import OutlinedInput from '@mui/material/OutlinedInput'
+
+
+
+export default function SearchBar() {
+
+    return(
+        <div style={{display: "flex", flexDirection: "column", backgroundColor:"white"}}>
+            <OutlinedInput/>
+        </div>
+    )
+
+}
