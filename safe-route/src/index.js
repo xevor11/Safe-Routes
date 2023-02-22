0c8c33c50fded4459c344836ab5883ef5b2411bc
ngentekis <ngentekis@gmail.com>
2023-02-21 19:58:39 -0600

    import fix

--- safe-route/src/index.js
+++ safe-route/src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import { Auth0Provider } from '@auth0/auth0-react';
