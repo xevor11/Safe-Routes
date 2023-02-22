1c231e1b6fc2cae7afcfa0eef9c4d934d5ad88ae
ngentekis <ngentekis@gmail.com>
2023-02-21 19:59:20 -0600

    import css to button

--- safe-route/src/components/login-button.js
+++ safe-route/src/components/login-button.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useAuth0 } from '@auth0/auth0-react';
+import './button.css'
 
 const LoginButton = () => {
 
@@ -7,7 +8,7 @@ const LoginButton = () => {
     return (
         !isAuthenticated && (
         <button
-            className='loginbutton'
+            className="loginbutton"
             onClick={() => loginWithRedirect()}
         >
             Login
