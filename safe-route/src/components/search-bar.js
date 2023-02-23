8b1a3fb44006a476092351cf93de33240212cf3e
ngentekis <ngentekis@gmail.com>
2023-02-22 21:38:42 -0600

    geosearch in progress

--- safe-route/src/components/search-bar.js
+++ safe-route/src/components/search-bar.js
@@ -1,13 +1,42 @@
 import React from "react";
-import OutlinedInput from '@mui/material/OutlinedInput'
+import { OutlinedInput, Button } from '@mui/material'
+import { height } from "@mui/system";
+import { GeocoderAutocomplete, GeoapifyContext } from '@geoapify/geocoder-autocomplete'
 
 
 
 export default function SearchBar() {
 
-    return(
-        <div style={{display: "flex", flexDirection: "column", backgroundColor:"white"}}>
-            <OutlinedInput/>
+    function onPlaceSelect(value) {
+        console.log(value);
+    }
+
+    function onSuggestionChange(value) {
+        console.log(value);
+    }
+
+    return (
+        <div style={{ display: "flex", flexDirection: "column" }}>
+            <div>
+                <div style={{ backgroundColor: "white", height: "50%", color: "black" }}>
+                    <GeoapifyContext>
+                        <GeocoderAutocomplete
+                            placeholder="search address"
+                            type="street"
+                            lang="en"
+                            limit="10"
+                            countryCodes="us"
+
+                        >
+
+                        </GeocoderAutocomplete>
+                    </GeoapifyContext>
+
+                </div>
+                {/* <Button variant="contained" color="primary">
+                    Search
+                </Button> */}
+            </div>
         </div>
     )
 
