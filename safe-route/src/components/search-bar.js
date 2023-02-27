0a55efbe303b7d8e536135975670443124331750
Mitchell-Vang <vang638@uwm.edu>
2023-02-27 17:48:17 -0600

    New Update to Search-bar.js

--- safe-route/src/components/search-bar.js
+++ safe-route/src/components/search-bar.js
@@ -1,8 +1,9 @@
 import React from "react";
-import { OutlinedInput, Button } from '@mui/material'
-import { height } from "@mui/system";
-import { GeocoderAutocomplete, GeoapifyContext } from '@geoapify/geocoder-autocomplete'
-
+//import { useState } from 'react';
+//import { OutlinedInput, Button } from '@mui/material'
+//import { height } from "@mui/system";
+import { GeoapifyGeocoderAutocomplete, GeoapifyContext } from '@geoapify/react-geocoder-autocomplete';
+//import '@geoapify/geocoder-autocomplete/styles/minimal.css'
 
 
 export default function SearchBar() {
@@ -19,17 +20,18 @@ export default function SearchBar() {
         <div style={{ display: "flex", flexDirection: "column" }}>
             <div>
                 <div style={{ backgroundColor: "white", height: "50%", color: "black" }}>
-                    <GeoapifyContext>
-                        <GeocoderAutocomplete
+                    <GeoapifyContext apiKey = "61d80f898ec14823899e64c8324a3f40">
+                        <GeoapifyGeocoderAutocomplete
                             placeholder="search address"
                             type="street"
                             lang="en"
                             limit="10"
                             countryCodes="us"
-
+                            placeSelect={onPlaceSelect}
+                            suggestionsChange={onSuggestionChange}
                         >
 
-                        </GeocoderAutocomplete>
+                        </GeoapifyGeocoderAutocomplete>
                     </GeoapifyContext>
 
                 </div>
