22b791d36b0e458b91fe38a65a5720651ec43e55
ngentekis <ngentekis@gmail.com>
2023-02-27 20:25:16 -0600

    added dynamic location

--- safe-route/src/App.js
+++ safe-route/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import { React, useState }from 'react';
 import './App.css';
 import LoginButton from './components/login-button';
 import LogoutButton from './components/logout-button';
@@ -7,15 +7,27 @@ import SignupButton from './components/signup-button';
 
 function App() {
 
+  const [coords, setCoords] = useState([43.075647, -87.886633]);
+
+  const successCallback = (position) => {
+    setCoords([position.coords.latitude, position.coords.longitude])
+  }
+  
+  const errorCallback = (error) => {
+    console.log(error);
+  }
+
+  const options = {
+    enableHighAccuracy: false,
+    timeout: 10000,
+  }
+
+  if(navigator.geolocation){
+    navigator.geolocation.getCurrentPosition(successCallback, errorCallback, options);
+  }
 
   return (
-    
-    <>
-      <LoginButton/>
-      <LogoutButton/>
-      <SignupButton/>
-      <MapCont/>
-    </>
+      <MapCont lat={coords[0]} lng={coords[1]}/>
   );  
 }
 
--- safe-route/src/components/recenter.js
+++ safe-route/src/components/recenter.js
@@ -2,11 +2,6 @@ import React, { useEffect } from "react";
 import { useMap } from "react-leaflet";
 
 
-const recenter = ({lat, lng}) => {
-    const map = useMap();
 
-    useEffect(() => {
-        map.setView([lat, lng]);
-    }, [lat, lng]);
-    return null;
-}
+
+export default { Recenter }
