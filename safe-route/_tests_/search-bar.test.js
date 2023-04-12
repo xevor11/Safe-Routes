6371850773d1028c64af96bca4282da5c22268e2
Vtewari2311 <vtewari@uwm.edu>
2023-04-11 22:04:58 -0500

    Added more tests for search bar

--- safe-route/_tests_/search-bar.test.js
+++ safe-route/_tests_/search-bar.test.js
@@ -18,4 +18,30 @@ describe('SearchBar', () => {
     fireEvent.keyDown(searchBar, { key: 'Enter' });
     expect(onPlaceSelect).toHaveBeenCalledWith(expect.objectContaining({ properties: { formatted: address } }));
   });
+
+  test('disables search input when loading prop is true', () => {
+    const { getByPlaceholderText } = render(<SearchBar loading={true} />);
+    const searchBar = getByPlaceholderText('search address');
+    expect(searchBar).toBeDisabled();
+  });
+
+  test('clears search input when clear button is clicked', () => {
+    const { getByPlaceholderText, getByLabelText } = render(<SearchBar />);
+    const searchBar = getByPlaceholderText('search address');
+    const clearButton = getByLabelText('clear search');
+    const address = '123 Main St';
+    fireEvent.change(searchBar, { target: { value: address } });
+    expect(searchBar.value).toBe(address);
+    fireEvent.click(clearButton);
+    expect(searchBar.value).toBe('');
+  });
+
+  test('calls onSearchChange when search input changes', () => {
+    const onSearchChange = jest.fn();
+    const { getByPlaceholderText } = render(<SearchBar onSearchChange={onSearchChange} />);
+    const searchBar = getByPlaceholderText('search address');
+    const address = '123 Main St';
+    fireEvent.change(searchBar, { target: { value: address } });
+    expect(onSearchChange).toHaveBeenCalledWith(address);
+  });
 });
