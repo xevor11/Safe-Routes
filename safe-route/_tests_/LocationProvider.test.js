a148b7ba4d6a5b20bf132f49a459fe0cc7ef4a83
Vtewari2311 <vtewari@uwm.edu>
2023-04-11 21:29:15 -0500

    Added tests for Location Provider Functionality

--- safe-route/_tests_/LocationProvider.test.js
+++ safe-route/_tests_/LocationProvider.test.js
@@ -40,4 +40,34 @@ describe('LocationProvider', () => {
       lng: -73.968285
     });
   });
+
+  test('useUserLocationUpdate updates userCoords in location state', () => {
+    const wrapper = ({ children }) => <LocationContextProvider>{children}</LocationContextProvider>;
+    const { result } = renderHook(() => useUserLocationUpdate(), { wrapper });
+
+    act(() => {
+      result.current({ lat: 51.509865, lng: -0.118092 });
+    });
+
+    const { result: locationResult } = renderHook(() => useLocation(), { wrapper });
+    expect(locationResult.current.location.userCoords).toEqual({
+      lat: 51.509865,
+      lng: -0.118092
+    });
+  });
+
+  test('useDestLocationUpdate updates destCoords in location state', () => {
+    const wrapper = ({ children }) => <LocationContextProvider>{children}</LocationContextProvider>;
+    const { result } = renderHook(() => useDestLocationUpdate(), { wrapper });
+
+    act(() => {
+      result.current({ lat: 40.785091, lng: -73.968285 });
+    });
+
+    const { result: locationResult } = renderHook(() => useLocation(), { wrapper });
+    expect(locationResult.current.location.destCoords).toEqual({
+      lat: 40.785091,
+      lng: -73.968285
+    });
+  });
 });
