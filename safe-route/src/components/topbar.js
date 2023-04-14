861a33448c6bcd3fa01b16a9560ce6180be67c28
ngentekis <ngentekis@gmail.com>
2023-04-13 21:55:16 -0500

    remove unused imports

--- safe-route/src/components/topbar.js
+++ safe-route/src/components/topbar.js
@@ -1,9 +1,5 @@
-import React, { useState } from "react"
 import AuthenticationButton from './authentication-button'
 import './topbar.css'
-import { IconButton, Menu, MenuItem, ToggleButton } from "@mui/material"
-import DensityMediumIcon from '@mui/icons-material/DensityMedium';
-import { useThemeUpdate } from "./theme";
 import UserSettingButton from "./usersetting-button";
 
 export default function TopBar() {
@@ -12,7 +8,7 @@ export default function TopBar() {
         <nav className="bar">SafeRoute
 
             <UserSettingButton></UserSettingButton>
-            
+
             <AuthenticationButton />
         </nav>
     )
