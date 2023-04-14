efeff669b0e08d99b731519ba0ff9be45c6691cc
alexschara <60195746+alexschara@users.noreply.github.com>
2023-04-13 19:23:41 -0500

    Options button only shows when user is authenticated now

--- safe-route/src/components/usersetting-button.js
+++ safe-route/src/components/usersetting-button.js
@@ -3,13 +3,29 @@ import LoginButton from './login-button';
 import SignupButton from './signup-button';
 import { useAuth0 } from '@auth0/auth0-react';
 import {Button} from "@mui/material";
+import ReactDropdown from "react-dropdown";
 
 const UserSettingButton = () => {
     const { loginWithRedirect, isAuthenticated } = useAuth0();
+    const [open, setOpen] = React.useState(false);
+
+    const handleOpen = () => {
+        setOpen(!open);
+    };
+
+    const handleMenuOne = () => {
+        // do stuff
+        setOpen(false);
+    };
+
+    const handleMenuTwo = () => {
+        // do stuff
+        setOpen(false);
+    };
     return (
         isAuthenticated && (
             <Button variant="contained" color="primary"
-                    //onClick={}
+                    //onClick={() => ()}
             >
                 Options
             </Button>
@@ -17,5 +33,18 @@ const UserSettingButton = () => {
 
     );
 };
-
+const Dropdown = ({ open, trigger, menu }) => {
+    return (
+        <Button className="Options">
+            {trigger}
+            {open ? (
+                <ul className="menu">
+                    {menu.map((menuItem, index) => (
+                        <li key={index} className="menu-item">{menuItem}</li>
+                    ))}
+                </ul>
+            ) : null}
+        </Button>
+    );
+};
 export default UserSettingButton;
