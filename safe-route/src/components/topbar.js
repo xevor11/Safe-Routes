a87c9854551cf1ee57c17c4f967d024624e0a04e
ngentekis <ngentekis@gmail.com>
2023-04-21 22:24:39 -0500

    mobile fixed

--- safe-route/src/components/topbar.js
+++ safe-route/src/components/topbar.js
@@ -1,15 +1,20 @@
 import AuthenticationButton from './authentication-button'
 import './topbar.css'
 import SettingsMenu from "./Settingsmenu";
+import { useMediaQuery } from '@mui/material';
 
 export default function TopBar() {
 
+    const media = useMediaQuery('(max-width:600px)');
+
+    const safeRoute = media ? '' : 'SafeRoute';
+
     return (
-        <nav className="bar">SafeRoute
+        <nav className="bar">{safeRoute}
 
             <SettingsMenu className="menu"></SettingsMenu>
 
-            <AuthenticationButton />
+            <AuthenticationButton id="authButton"/>
         </nav>
     )
 }
