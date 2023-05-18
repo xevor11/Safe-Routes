1def65f64249ae53377b60b0ba21df2b5570a3d1
ngentekis <ngentekis@gmail.com>
2023-05-18 18:35:16 -0500

    updated logout button styling

--- safe-route/src/components/logout-button.js
+++ safe-route/src/components/logout-button.js
@@ -3,10 +3,14 @@ import { useAuth0 } from '@auth0/auth0-react';
 import { Button, styled } from '@mui/material';
 
 
-const StyledButton = styled(Button)({
-  width: '3vw',
-})
-
+const StyledButton = styled(Button)(({ theme }) => ({
+    
+  [theme.breakpoints.down('sm')]: {
+    
+    width: '4vw', 
+    padding: '0.5em', 
+  },
+}));
 
 const LogoutButton = () => {
   const { logout, isAuthenticated } = useAuth0();
