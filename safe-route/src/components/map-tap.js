799afac487670fc2d62267836992bb3c7bd44814
ngentekis <ngentekis@gmail.com>
2023-05-18 19:21:14 -0500

    simplify

--- safe-route/src/components/map-tap.js
+++ safe-route/src/components/map-tap.js
@@ -1,12 +1,9 @@
 import { useMap } from "react-leaflet";
-import {useLocation, useCenteredUpdate, useCenterLocationUpdate } from "./LocationProvider";
+import { useCenteredUpdate, useCenterLocationUpdate } from "./LocationProvider";
 
 
 const MapTap = () => {
     const map = useMap()
-    const getLocationHook = useLocation()
-    //extrapolate object from useContext hook
-    const location = getLocationHook.location
     const useCenter = useCenteredUpdate()
     const useCenterUpdate = useCenterLocationUpdate()
 
